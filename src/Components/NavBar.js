e52fce8d8881db13f497bb9585ca1e476ecef6e2
Edwin Codrington <edwincodrington@gmail.com>
2022-06-15 10:21:45 -0400

    more merge conflicts fixed

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,24 +1,11 @@
-
 import { Link } from "react-router-dom";
-
 
 export const NavBar = () => {
 	return (
 		<header>
-
 			Youtube &nbsp;
-			<Link to='/'>Home</Link> &nbsp;
-			<Link to='/about'>About</Link>
-
-			<article>
-				<h3>
-					<Link to="/">Home</Link>
-				</h3>
-				<h3>
-					<Link to="/about/">About</Link>
-				</h3>
-			</article>
-
+			<Link to="/">Home</Link> &nbsp;
+			<Link to="/about">About</Link>
 		</header>
 	);
 };
